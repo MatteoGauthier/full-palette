f90877ae29310bd84b1da96b10ff228814a8fd92
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-11-22 11:40:36 +0100

    Adding all palettes

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+theme: {
+    colors: {
+      'true-gray': colors.trueGray,
+      'cool-gray': colors.coolGray,
+      'blue-gray': colors.blueGray,
+      'warm-gray': colors.warmGray,
+      indigo: colors.indigo,
+      rose: colors.rose,
+      amber: colors.amber,
+      fuchsia: colors.fuchsia,
+      emerald: colors.emerald,
+      'light-blue': colors.lightBlue,
+      lime: colors.lime,
+      teal: colors.teal
+    },
+  },
+}
