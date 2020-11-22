ea65af1432c8ede5cb622c85a4c1a682db10fd41
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-11-22 12:04:12 +0100

    change tailwind config to extend mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
-theme: {
-    colors: {
-      'true-gray': colors.trueGray,
-      'cool-gray': colors.coolGray,
-      'blue-gray': colors.blueGray,
-      'warm-gray': colors.warmGray,
-      indigo: colors.indigo,
-      rose: colors.rose,
-      amber: colors.amber,
-      fuchsia: colors.fuchsia,
-      emerald: colors.emerald,
-      'light-blue': colors.lightBlue,
-      lime: colors.lime,
-      teal: colors.teal
+  theme: {
+    extend: {
+      colors: {
+        "true-gray": colors.trueGray,
+        "cool-gray": colors.coolGray,
+        "blue-gray": colors.blueGray,
+        "warm-gray": colors.warmGray,
+        indigo: colors.indigo,
+        rose: colors.rose,
+        amber: colors.amber,
+        fuchsia: colors.fuchsia,
+        emerald: colors.emerald,
+        "light-blue": colors.lightBlue,
+        lime: colors.lime,
+        teal: colors.teal,
+      },
     },
   },
-}
+};
