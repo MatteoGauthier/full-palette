2ac920a40a4d28533b7ab6aec6a3b9ee4316b993
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-07-13 00:14:58 +0200

    adding new color palette of tailwind v2.2.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        "true-gray": colors.trueGray,
-        "cool-gray": colors.coolGray,
-        "blue-gray": colors.blueGray,
-        "warm-gray": colors.warmGray,
-        indigo: colors.indigo,
-        rose: colors.rose,
-        amber: colors.amber,
-        fuchsia: colors.fuchsia,
-        emerald: colors.emerald,
-        "light-blue": colors.lightBlue,
-        lime: colors.lime,
-        teal: colors.teal,
-      },
-    },
-  },
+	theme: {
+		extend: {
+			colors: {
+				rose: colors.rose,
+				pink: colors.pink,
+				fuchsia: colors.fuchsia,
+				purple: colors.purple,
+				violet: colors.violet,
+				indigo: colors.indigo,
+				blue: colors.blue,
+				sky: colors.sky,
+				cyan: colors.cyan,
+				teal: colors.teal,
+				emerald: colors.emerald,
+				green: colors.green,
+				lime: colors.lime,
+				yellow: colors.yellow,
+				amber: colors.amber,
+				orange: colors.orange,
+				red: colors.red,
+				"warm-gray": colors.warmGray,
+				"true-gray": colors.trueGray,
+				gray: colors.gray,
+				"cool-gray": colors.coolGray,
+				"blue-gray": colors.blueGray,
+			},
+		},
+	},
 };
